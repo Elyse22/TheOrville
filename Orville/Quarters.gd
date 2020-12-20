f78666410a36354ca29dec562ff6d59d5428ae1f
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2020-12-20 19:11:37 +0500

    Dialog Player Improved

--- Orville/Quarters.gd
+++ Orville/Quarters.gd
@@ -5,15 +5,5 @@ onready var dialog_player = $HUD/DialogPlayer
 
 
 func _ready():
-	dialog_player.play("Quarters")
+	dialog_player.play()
 
-
-func entered_plant_1(body):
-	if body.name == "Player":
-		dialog_player.play("Plant_1")
-
-
-func exited_pant_1(body):
-	if body.name == "Player":
-		if dialog_player.current_dialog == "Plant_1":
-			dialog_player.stop()
--- Orville/Src/DialogPlayer/DialogPlayer.gd
+++ Orville/Src/DialogPlayer/DialogPlayer.gd
@@ -1,51 +1,49 @@
 extends Control
 
-export (Dictionary) var dialogs = {}
+export (Array, String) var speakers
+export (Array, String) var dialogs
+signal stopped
 var index = -1
-var message_list = []
-var speaker_list = []
-var current_dialog = ""
 
 
 func _ready():
 	return
-	print(dialogs)
-	play("Quarters")
+	play()
 
 
-func play(dialog):
+func play():
+	index = -1
 	show()
-	current_dialog = dialog
-	var dialogs_list = []
-	for key in dialogs[dialog].keys():
-		var message = key + ": " + dialogs[dialog][key]
-		print(message)
-		dialogs_list.append(message)
-		speaker_list.append(key)
-		message_list.append(dialogs[dialog][key])
 	next_dialog()
 
 
 func next_dialog():
-	if index >= speaker_list.size() - 1:
-		hide()
+	if index >= speakers.size() - 1:
+		stop()
+		return
+	if dialogs[index+1] == "":
+		stop()
 		return
 	index += 1
-	$Speaker/Name.text = speaker_list[index]
-	$Message.text = message_list[index]
+	$Speaker/Name.text = speakers[index]
+	$Message.text = dialogs[index]
 	$Message.percent_visible = 0
 	$Tween.interpolate_property(
 		$Message, "percent_visible", 0,1,0.5,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
 	)
 	$Tween.start()
-	if speaker_list[index] != "MESSAGE":
-		var file_name = str(speaker_list[index]).replace(" ", "")
-		#$Speaker/Icon.texture = load("res://Assets/AllCharacters/Characters/" + file_name + ".png")
-		$Speaker/Icon.texture = null
+	if speakers[index] != "MESSAGE":
+		var file_name = str(speakers[index]).replace(" ", "")
+		var file = File.new()
+		if file.file_exists("res://Assets/AllCharacters/Face/" + file_name + ".png"):
+			$Speaker/Icon.texture = load("res://Assets/AllCharacters/Face/" + file_name + ".png")
+		else:
+			$Speaker/Icon.texture = null
 	else:
 		$Speaker/Icon.texture = null
 		$Speaker/Name.text = ""
+
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
@@ -57,4 +55,6 @@ func tween_completed(object, key):
 
 
 func stop():
+	emit_signal("stopped")
+	index = -1
 	hide()
