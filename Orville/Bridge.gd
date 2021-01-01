d35c569dda301b130a04ddab9ee045ae1e596d3e
Elyse <elysehargreaves@hotmail.com>
2021-01-01 21:32:30 +1000

    Added Cutscene1, tried connecting to Bridge End NPC Dialgoue

--- Orville/Bridge.gd
+++ Orville/Bridge.gd
@@ -5,3 +5,10 @@ func _ready():
 	if Data.current_scene == filename:
 		$YSort/Player.position = Data.player_position
 	Data.current_scene = filename
+	
+
+
+func _on_DialogPlayer_stopped(): 
+	get_tree().change_scene("res://Cutscene1.tscn")
+
+	
