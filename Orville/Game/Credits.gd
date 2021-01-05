582a88719670eb92d46a3133b3980d9a902b0cb1
Nathan Franke <natfra@pm.me>
2021-01-05 04:16:54 -0600

    Credits Boilerplate

---
+++ Orville/Game/Credits.gd
@@ -0,0 +1,11 @@
+extends Control
+
+onready var text = $Label
+
+func _ready():
+	text.rect_position.y = get_viewport().size.y
+
+func _process(delta):
+	text.rect_position.y -= 50.0 * delta
+	if text.rect_position.y < -rect_size.y:
+		get_tree().quit()
